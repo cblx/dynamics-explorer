e248eb46f0af35a2b64a7b8a461cb6d4bc2d1934
Guilherme Meinlschmiedt Abdo <guilherme.abdo@codeblox.com.br>
2023-02-05 20:32:13 -0300

    load an optiona appsettings.Local.json in Demo startup

--- Cblx.Dynamics.Explorer.Demo/Program.cs
+++ Cblx.Dynamics.Explorer.Demo/Program.cs
@@ -2,6 +2,7 @@ using Cblx.Dynamics.Explorer;
 using Cblx.Dynamics.Explorer.Demo.Mappings;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile("appsettings.Local.json", optional: true, reloadOnChange: true);
 builder.Services.AddDynamicsExplorer(
     // mapping assemblies
     typeof(TbAccount).Assembly
