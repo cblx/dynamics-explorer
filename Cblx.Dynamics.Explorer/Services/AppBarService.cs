eede36843725a7d01954b5f2615de2851aa5db80
Guilherme Meinlschmiedt Abdo <guilherme.abdo@codeblox.com.br>
2023-08-07 11:34:51 -0300

    🩹fix appbar behavior

--- Cblx.Dynamics.Explorer/Services/AppBarService.cs
+++ Cblx.Dynamics.Explorer/Services/AppBarService.cs
@@ -4,16 +4,12 @@ namespace Cblx.Dynamics.Explorer.Services;
 
 public class AppBarService
 {
-    private RenderFragment? _content;
-
-    public event Action? OnContentChanged;
-
-    public RenderFragment? Content { 
-        get => _content; 
-        set
-        {
-            _content = value;
-            OnContentChanged?.Invoke();
-        }
+    public Action? ContentChanged { get; set; }
+    public RenderFragment? Content { get; private set; }
+    public void SetContent(RenderFragment? content)
+    {
+        Content = content;
+        if(ContentChanged is null) { return; }
+        ContentChanged();
     }
 }
