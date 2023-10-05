1dbc7249eeced2bd27ee504e5a98afa5faf52419
Guilherme Meinlschmiedt Abdo <guilherme.abdo@codeblox.com.br>
2023-10-05 11:26:21 -0300

    dialog com erro do back

--- NET8/Cblx.Dynamics.Explorer/Cblx.Dynamics.Explorer.Client/Services/DynamicsServices/Data/PatchItem/PatchItemClient.cs
+++ NET8/Cblx.Dynamics.Explorer/Cblx.Dynamics.Explorer.Client/Services/DynamicsServices/Data/PatchItem/PatchItemClient.cs
@@ -1,10 +1,18 @@
 ﻿
 using System.Net.Http.Json;
+using System.Text.Json.Nodes;
 
 namespace Cblx.Dynamics.Explorer.Client.Services.DynamicsServices.Data.PatchItem;
 
 public class PatchItemClient(HttpClient client) : IPatchItem
 {
     public async Task ExecuteAsync(PatchItemRequest request)
-        => await client.PatchAsJsonAsync(Route.GetEndpoint<IPatchItem>(), request);
+    {
+        var response = await client.PatchAsJsonAsync(Route.GetEndpoint<IPatchItem>(), request);
+        if (!response.IsSuccessStatusCode)
+        {
+            var responseJson = await response.Content.ReadFromJsonAsync<JsonObject>();
+            throw new InvalidOperationException(responseJson?["error"]?.ToString() ?? "Unexpected error");
+        }
+    }
 }
