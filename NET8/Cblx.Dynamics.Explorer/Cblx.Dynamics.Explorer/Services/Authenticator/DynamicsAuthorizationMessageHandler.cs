7e81f3811e8ea9b3774b2d5125f7c3c2b3e6f3da
Guilherme Meinlschmiedt Abdo <guilherme.abdo@codeblox.com.br>
2023-10-19 22:06:55 -0300

    substitui lib obsoleta pela recomendada

--- NET8/Cblx.Dynamics.Explorer/Cblx.Dynamics.Explorer/Services/Authenticator/DynamicsAuthorizationMessageHandler.cs
+++ NET8/Cblx.Dynamics.Explorer/Cblx.Dynamics.Explorer/Services/Authenticator/DynamicsAuthorizationMessageHandler.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.Extensions.Caching.Memory;
-using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Identity.Client;
 using System.Net.Http.Headers;
 
 namespace Cblx.Dynamics.Explorer.Services.Authenticator;
@@ -11,11 +11,14 @@ public class DynamicsAuthorizationMessageHandler(DynamicsConfig config, IMemoryC
         var authorizationHeader = await memoryCache.GetOrCreateAsync(config.Name, async (entry) =>
         {
             Console.WriteLine($"*** ACQUIRING NEW TOKEN FOR {config.Name} ***");
-            var authenticationContext = new AuthenticationContext(config.Authority);
-            var credential = new ClientCredential(config.ClientId, config.ClientSecret);
-            var authentication = await authenticationContext.AcquireTokenAsync(config.ResourceUrl, credential);
-            entry.AbsoluteExpiration = authentication.ExpiresOn;
-            return AuthenticationHeaderValue.Parse($"Bearer {authentication.AccessToken}");
+            var app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
+                .WithClientSecret(config.ClientSecret)
+                .WithAuthority(config.Authority)
+                .Build();
+            // Estudar sobre token caches. Ainda não entendi => https://learn.microsoft.com/pt-br/entra/msal/dotnet/how-to/token-cache-serialization?tabs=aspnetcore
+            var authResult = await app.AcquireTokenForClient([$"{config.ResourceUrl}/.default"]).ExecuteAsync();
+            entry.AbsoluteExpiration = authResult.ExpiresOn;
+            return AuthenticationHeaderValue.Parse($"Bearer {authResult.AccessToken}");
         });
         request.Headers.Authorization = authorizationHeader;
         return await base.SendAsync(request, cancellationToken);
